a9dfad494c8c2e5743128048abdb2c61a188777e
Eve Stockstill <evestockstillpdx@gmail.com>
2019-12-16 23:48:09 -0800

    color

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@
 const dotenv = require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors');
+// const cors = require('cors');
 
 const app = express();
 app.use(morgan('dev'));
