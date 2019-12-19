dbb23503da142d4c3dc1d2a2069356cb719d2427
Eve Stockstill <evestockstillpdx@gmail.com>
2019-12-19 14:03:12 -0800

    heroku

--- public/RecipeHome/RecipeItem.js
+++ public/RecipeHome/RecipeItem.js
@@ -3,15 +3,15 @@ import Component from '../Component/Component.js';
 class RecipeItem extends Component {
   renderHTML() {
     const recipe = this.props.recipe;
-    
+    let image = recipe.url_image;
     if(image === 'N/A') {
-      image = '';
+      image = '.public/assets/herodessert.jpeg';
     }
     return `
         <li class="recipe-item">
     <div>
     <a href="http://${recipe.name}" target="_blank"></a>
-       
+        <img src="${image}" alt="${recipe.name}>
             <div class="recipe-cards">
                 <h2 class="recipe-name">${recipe.name}</h2>
                 </div>
